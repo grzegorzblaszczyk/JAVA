e01675d95e37a1c869a2529022db40e270980ea2
Antoni Urbaniak <38950013+StrollingOx@users.noreply.github.com>
2018-05-15 15:49:57 +0200

    Update load_and_save.java

--- exercisesJava/load&save/load_and_save.java
+++ exercisesJava/load&save/load_and_save.java
@@ -6,9 +6,10 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import javax.swing.JFileChooser;
 
-public class load_and_save 
+public class load_and_save
 {   
 	//BRAKUJE OBSŁUGI WYJĄTKÓW
+	//POPRAW LOAD
 	public static int load(JFileChooser chooser, int index, String[][] table) 
 	{
 		Scanner odczyt=null;
@@ -43,9 +44,23 @@ public class load_and_save
 		odczyt.close();
 		return index;
 	}
-
+	public static double convertTime(int index, String[][] table) 
+	{
+		double ms, s, m, h;
+		String[] parts = table[index][1].split("[.]");
+		String[] parts2 = parts[0].split(":");
+		h = Double.parseDouble(parts2[0])*3600;
+		m = Double.parseDouble(parts2[1])*60;
+		s = Double.parseDouble(parts2[2]);
+		ms = Double.parseDouble(parts[1])/1000;
+		return h+m+s+ms;
+	}
 	public static void save(String fileName, int index, String[][] table) 
 	{   
+		double time = 0.0;
+		double distance = 0.0;
+		double velocity = 0.0;
+		double hight = 0.0;
 		PrintWriter outputStream = null;                                                 
 		try 
 		{
@@ -56,32 +71,38 @@ public class load_and_save
 			System.out.println ("Error while creating " +fileName);
 			System.exit (0); 
 		}
-		outputStream.println("Blah, blah, blah. Blah."); 
-		for (int ix=0;ix<index;ix++)
+		outputStream.printf("T[s]\t\tV[m/s]\t\tH[m]\n"); 
+		outputStream.printf("0.000\t\t0.000\t\t%.1f\n", Double.parseDouble(table[0][4])); 
+		for (int ix=0;ix<(index-1);ix++)
 		{                       
-			outputStream.printf("%s\t%s\t%s\n",table[ix][1],table[ix][4],table[ix][5] );
+			time = load_and_save.convertTime(ix+1,table)-load_and_save.convertTime(ix,table);
+			distance = (Double.parseDouble(table[ix+1][5]) - Double.parseDouble(table[ix][5]))*1000;
+			velocity = time*distance;
+			hight = Double.parseDouble(table[ix+1][4]);
+			outputStream.printf("%.3f\t\t%.3f\t\t%.1f\n",time,velocity,hight);
 		}
 		System.out.println("Data saved to " +fileName);
 		outputStream.close();			
 	}
 	
+	
+	
 	public static void main(String[] args) throws FileNotFoundException 
 	{	
 		JFileChooser chooser = new JFileChooser();
 		int returnVal = chooser.showOpenDialog(null);
-		if(returnVal == JFileChooser.APPROVE_OPTION) {
-		System.out.println(chooser.getSelectedFile().getName() + " chosen.");
+		if(returnVal == JFileChooser.APPROVE_OPTION) 
+			System.out.println(chooser.getSelectedFile().getName() + " chosen.");
+		else 
+			System.exit(0);
 		
-		int i = 0;
+		int index = 0;
 		String[][] tablica = new String[2300][6]; //[x][0]date, [x][1]time, [x][2]lati, [x][3]longi, [x][4]alti, [x][5]dist;
 		
-		
-		i = load_and_save.load(chooser, i, tablica); // method 'load' must be int so it can return index value
-		load_and_save.save("velocity.csv", i, tablica);
-	
-		}
-		System.exit(0);
+		index = load_and_save.load(chooser, index, tablica); // method 'load' must be int so it can return index value
+		load_and_save.save("velocity.csv", index, tablica);
 	}
+	
 	
 
 	
