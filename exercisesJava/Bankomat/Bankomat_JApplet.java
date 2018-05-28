36c997de195209e0b3244f3adc0a712e10177d56
Antoni Urbaniak <38950013+StrollingOx@users.noreply.github.com>
2018-05-28 18:43:19 +0200

    Update Bankomat_JApplet.java

--- exercisesJava/Bankomat/Bankomat_JApplet.java
+++ exercisesJava/Bankomat/Bankomat_JApplet.java
@@ -51,7 +51,7 @@ public class Bankomat_JApplet extends JApplet implements ActionListener
 	
 	public void init()
 	{
-		setSize(800,400);
+		setSize(200,400);
 		setVisible(true);
 		setLayout (null);
 	}
@@ -65,6 +65,7 @@ public class Bankomat_JApplet extends JApplet implements ActionListener
 		add(tf);
 		dodajKlawiature(tf);
 		
+		
 		JButton clickMeEnter = new JButton("e");
 		clickMeEnter.setBounds(130,280,60,60);
 		clickMeEnter.addActionListener( 
@@ -72,51 +73,14 @@ public class Bankomat_JApplet extends JApplet implements ActionListener
 				{
 					public void actionPerformed(ActionEvent whatsUp)
 					{	
+						JFrame f = new JFrame("SUmthing");
+	
+						MyClass a = new MyClass(tf);
+						f.getContentPane().add(a);
+						f.setSize(800, 300);
+						f.setVisible(true);
 						
-						/*	
-						int [] transakcja = {1,0,0,0,0,0,0,0}; //8
-						int x = 200;
-						int y = 15;
-						int kwota = 100;
-						String [] nominals_US= {"  $500","  $200","  $100","   $50","   $20","   $10","    $5","    $1"};
-						final int[] nominals = {500, 200, 100, 50, 20, 10, 5, 1};
-					    int index=0;
-					    kwota = Integer.parseInt(tf.getText());
-					    System.out.println("Start: $"+kwota);
-					    while(kwota!=0.0)
-					    {
-					    	if((kwota-nominals[index])>=0)
-					    	{
-					    		kwota = kwota - nominals[index];
-					    		transakcja[index] = transakcja[index]+1;
-					    	}
-					    	else
-					    	{
-					    		index++;
-					    	}
-					    }
-					    for(int i : transakcja)
-								System.out.println(transakcja[i]);
-							
-						int start_x = x;
-						index=0;
-						for(index=0;index<8;index++)
-						{
-							for(int i = 0; i<transakcja[index];i++) 
-							{
-								g.setColor(Color.GREEN);
-								g.fillRect(x+(i*1), y+(i*10), 40, 20);
-								g.setColor(Color.BLACK);
-								g.drawRect(x+(i*1), y+(i*10), 40, 20);
-								g.drawString(nominals_US[index],x+(i*1),y+(15+i*10));
-								
-							}
-							x+=60;
-							index++;
-						}
-						x = start_x; // u can also use "x-=(60*index) otherwise the program will start drawing dollars from x = 475 
-						repaint();
-					*/}
+					}
 				});
 		add(clickMeEnter);	
 	
@@ -127,4 +91,58 @@ public class Bankomat_JApplet extends JApplet implements ActionListener
 	public void actionPerformed(ActionEvent e) {
 	}
 
+}
+
+class MyClass extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	private int kwota;
+	private int [] transakcja = {0,0,0,0,0,0,0,0}; //8
+	private String [] nominals_US= {"  $500","  $200","  $100","   $50","   $20","   $10","    $5","    $1"};
+	private final int[] nominals = {500, 200, 100, 50, 20, 10, 5, 1};
+	private int index = 0;
+	
+	public MyClass(TextField tf)
+	{
+		this.kwota=Integer.parseInt(tf.getText());
+		
+	    System.out.println("Start: $"+kwota);
+	    while(kwota!=0.0)
+	    {
+	    	if((kwota-nominals[index])>=0)
+	    	{
+	    		kwota = kwota - nominals[index];
+	    		System.out.print("Kwota = " +kwota+"; ");
+	    		transakcja[index] = transakcja[index]+1;
+	    		System.out.println("transakcja["+index+"] = "+transakcja[index]+";");
+	    	}
+	    	else
+	    	{
+	    		index++;
+	    	}
+	    }
+	    	
+	}
+	
+	public void paint(Graphics g)
+	{
+		int x = 15, y = 15;
+		index=0;
+		for(index=0;index<8;index++)
+		{
+			for(int i = 0; i<transakcja[index];i++) 
+			{
+				g.setColor(Color.GREEN);
+				g.fillRect(x+(i*1), y+(i*10), 40, 20);
+				g.setColor(Color.BLACK);
+				g.drawRect(x+(i*1), y+(i*10), 40, 20);
+				g.drawString(nominals_US[index],x+(i*1),y+(15+i*10));
+				
+			}
+			x+=60;
+		}
+		x = 15; // u can also use "x-=(60*index) otherwise the program will start drawing dollars from x = 475 
+		
+	
+	}
 }
