737f20101e29a1adb768d674a66723ee8c29023d
Antoni Urbaniak <38950013+StrollingOx@users.noreply.github.com>
2018-06-04 21:52:46 +0200

    Add files via upload

---
+++ TULJava/taskDatabase/ProgramInterface.java
@@ -0,0 +1,21 @@
+package taskDatabase;
+
+public class ProgramInterface 
+{
+	
+	public void printProgramInterface()
+	{
+		System.out.println("       MENU");
+		System.out.println("--------------------------------");
+		System.out.println("      Hello!!   %d");
+		System.out.println("--------------------------------");
+		System.out.println("1|");
+		System.out.println("2|");
+		System.out.println("3|");
+		System.out.println("4|");
+		System.out.println("5|");
+		System.out.println("0|");
+		System.out.println(">>> ");
+
+	}
+}
