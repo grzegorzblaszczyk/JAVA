efbf5a572101e6ad26e9382fede42e0b1b46a2e4
Antoni Urbaniak <38950013+StrollingOx@users.noreply.github.com>
2018-06-14 06:45:32 +0200

    Update ProgramInterface.java

--- TULJava/taskDatabase/src/taskDatabase/ProgramInterface.java
+++ TULJava/taskDatabase/src/taskDatabase/ProgramInterface.java
@@ -13,7 +13,7 @@ public class ProgramInterface
 		System.out.println("1|'SELECT [columns] FROM [tableName]'");
 		System.out.println("2|'INSERT INTO [tableName] VALUES [values]'");
 		System.out.println("3|'UPDATE [tableName] SET [values] WHERE [condition]'");
-		System.out.println("-|");
+		System.out.println("4|'DELETE FROM [tableName] WHERE [condition]'");
 		System.out.println("5|'SHOW TABLE NAME'");
 		System.out.println("6|'SHOW INTERFACE'");
 		System.out.println("-|'(Q)uit'");
